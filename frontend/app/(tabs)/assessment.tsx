ff206ea5f7824db80c0a5c48d97933639c0054fd
Staphone Omondi <95640951+StaphoneWizzoh@users.noreply.github.com>
2025-03-04 19:04:38 +0300

    feat: Enhance Assessment screen with input fields for title, strand, and sub strand

--- frontend/app/(tabs)/assessment.tsx
+++ frontend/app/(tabs)/assessment.tsx
@@ -1,17 +1,65 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+
+import { Text, TextInput, View } from "react-native";
 import { ScreenWithBackground } from "./_layout";
 
 export default function Assessment() {
+    const [title, setTitle] = useState("");
+    const [strand, setStrand] = useState("");
+
     return (
         <ScreenWithBackground>
             <View
                 style={{
                     flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
+                    marginLeft: 16,
+                    marginRight: 16,
                 }}
             >
-                <Text>Edit app/assessment.tsx to edit this screen.</Text>
+                <View>
+                    <Text
+                        style={{
+                            color: "white",
+                            fontWeight: 700,
+                            fontSize: 20,
+                        }}
+                    >
+                        New Assessment
+                    </Text>
+
+                    <View>
+                        <Text style={{ color: "white" }}>Title</Text>
+                        <TextInput
+                            style={{ height: 40, padding: 5, color: "white" }}
+                            placeholder="Type here to translate!"
+                            onChangeText={(newText) => setTitle(newText)}
+                            defaultValue={title}
+                        />
+                    </View>
+
+                    <View>
+                        <Text style={{ color: "white" }}>Strand</Text>
+                        <TextInput
+                            style={{ height: 40, padding: 5, color: "white" }}
+                            placeholder="Type here to translate!"
+                            onChangeText={(newText) => setStrand(newText)}
+                            defaultValue={strand}
+                        />
+                    </View>
+
+                    <View>
+                        <Text style={{ color: "white" }}>Sub strand</Text>
+                        <TextInput
+                            style={{ height: 40, padding: 5, color: "white" }}
+                            placeholder="Type here to translate!"
+                            onChangeText={(newText) => setStrand(newText)}
+                            defaultValue={strand}
+                        />
+                    </View>
+                </View>
+                <View>
+                    <Text style={{ color: "white" }}>New Assessment</Text>
+                </View>
             </View>
         </ScreenWithBackground>
     );
